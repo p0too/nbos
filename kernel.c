c28498fb05ccf76f07b4a1ebf2673145e6f67e12
p0too <real.p0too@gmail.com>
2021-12-08 08:27:39 +0530

    kernel.c: Remove Erroneous Comment
    Previous two commits have that erroneous comment, which has been removed
    here.

--- kernel.c
+++ kernel.c
@@ -2,7 +2,6 @@ void main()
 {
   // the screen is 80x25 characters
   // want to write to second line
-  // so 0xb8000 + 80 which is 0xb8000 + 0x50 = 0xb8050
   int row = 1;
   int col = 0;
   char* video_memory = (char*) (0xb8000 + 2 * (row * 80 + col));
