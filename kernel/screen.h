c4413904f8dc1f4283dc3ebb28b983f5cc198ae5
p0too <real.p0too@gmail.com>
2021-12-08 18:55:06 +0530

    Port I/O fns And Sorta Screen Driver
    Some port i/o functions (in low_level.h) used by the screen driver enable
    more robust printing to screen by using its CTRL and DATA registers.

---
+++ kernel/screen.h
@@ -0,0 +1,24 @@
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+#define GREEN_ON_BLACK 0x02
+
+#define REG_SCREEN_CTRL 0x3D4
+#define REG_SCREEN_DATA 0x3D5
+
+void print_char(char character, int col, int row, char attribute_byte);
+
+int get_screen_offset();
+
+int get_cursor();
+
+void set_cursor(int offset);
+
+void print_at(char *message, int col, int row);
+
+void print(char *message);
+
+void clear_screen();
+
+// TODO:
+// void scroll_screen();
