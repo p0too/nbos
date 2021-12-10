26df6a3d571eec3b4efbdc1a1cbbd769fea298cc
p0too <real.p0too@gmail.com>
2021-12-10 09:54:48 +0530

    Add types.h
    Added types.h to be more specific about variable sizes.

---
+++ cpu/types.h
@@ -0,0 +1,13 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef int s32_t;
+typedef unsigned int u32_t;
+
+typedef short s16_t;
+typedef unsigned short u16_t;
+
+typedef char s8_t;
+typedef unsigned char u8_t;
+
+#endif
