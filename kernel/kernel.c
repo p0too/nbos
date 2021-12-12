90feb13ec6ad0a7d5badaa74153e8694c01e1f1e
p0too <real.p0too@gmail.com>
2021-12-12 10:20:23 +0530

    Keyboard is Operational Now :)
    Keyboard scancodes are received and printed on screen, for now.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,7 @@
 #include "../drivers/screen.h"
 #include "../cpu/idt.h"
 #include "../drivers/timer.h"
+#include "../drivers/keyboard.h"
 
 void main()
 {
@@ -10,6 +11,9 @@ void main()
   __asm__ __volatile__ ("sti");
 
   // initialise timer interrupt
-  init_timer(50);
+  //init_timer(50);
+
+  // keyboard interrupt
+  init_keyboard();
 }
 
